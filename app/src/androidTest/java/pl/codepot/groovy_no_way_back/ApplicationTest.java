786498e4fdf9d5545460b41fb4cd620666bdc4ca
ordon <ordon1313@gmail.com>
2015-08-23 10:18:24 +0200

    init

---
+++ app/src/androidTest/java/pl/codepot/groovy_no_way_back/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.codepot.groovy_no_way_back;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/pl/codepot/groovy_no_way_back/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.codepot.groovy_no_way_back;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
