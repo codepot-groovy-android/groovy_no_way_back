d0cb182e32187e293c7d8252475e407420d03c36
ordon <ordon1313@gmail.com>
2015-08-24 21:57:01 +0200

    ChooseUserActivity

---
+++ app/src/main/java/pl/codepot/groovy_no_way_back/ChooseUserActivity.java
@@ -0,0 +1,12 @@
+package pl.codepot.groovy_no_way_back;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public final class ChooseUserActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_user_activity);
+    }
+}
