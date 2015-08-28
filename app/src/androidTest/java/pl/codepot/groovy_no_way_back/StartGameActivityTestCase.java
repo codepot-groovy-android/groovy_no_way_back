c057de45671888cf458acd8b041d2cf6310de24b
Maciej Górski <maciek.gorski@gmail.com>
2015-08-28 08:06:32 +0200

    Make tests simpler to read.

--- app/src/androidTest/java/pl/codepot/groovy_no_way_back/StartGameActivityTestCase.java
+++ app/src/androidTest/java/pl/codepot/groovy_no_way_back/StartGameActivityTestCase.java
@@ -8,6 +8,10 @@ import android.test.ActivityInstrumentationTestCase2;
 import pl.codepot.groovy_no_way_back.dagger.Injector;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public final class StartGameActivityTestCase extends ActivityInstrumentationTestCase2<StartGameActivity> {
 
@@ -23,11 +27,11 @@ public final class StartGameActivityTestCase extends ActivityInstrumentationTest
     }
 
     public void testShouldStartChooseActivityAfterPlayClicked() {
-        onView(ViewMatchers.withId(R.id.play)).perform(ViewActions.click());
-        onView(ViewMatchers.withId(R.id.users_list_view)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.play)).perform(ViewActions.click());
+        onView(withId(R.id.users_list_view)).check(matches(isDisplayed()));
     }
 
     public void testShouldShowSavedBestScoreOnResume() {
-        onView(ViewMatchers.withId(R.id.best_score_view)).check(ViewAssertions.matches(ViewMatchers.withText("16")));
+        onView(withId(R.id.best_score_view)).check(matches(withText("16")));
     }
 }
