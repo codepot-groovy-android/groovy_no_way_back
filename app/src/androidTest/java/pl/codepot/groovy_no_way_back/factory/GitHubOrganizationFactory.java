1dea7c42d7f7a94b6e0226fc73428b218131ac2e
Maciej Górski <maciek.gorski@gmail.com>
2015-08-28 07:46:41 +0200

    [refactoring] Remove unnecessary fields.

--- app/src/androidTest/java/pl/codepot/groovy_no_way_back/factory/GitHubOrganizationFactory.java
+++ app/src/androidTest/java/pl/codepot/groovy_no_way_back/factory/GitHubOrganizationFactory.java
@@ -8,26 +8,9 @@ public final class GitHubOrganizationFactory {
         GitHubOrganization elPassion = new GitHubOrganization();
         elPassion.login = "elpassion";
         elPassion.id = 98255L;
-        elPassion.url = "https://api.github.com/orgs/elpassion";
-        elPassion.repos_url = "https://api.github.com/orgs/elpassion/repos";
-        elPassion.events_url = "https://api.github.com/orgs/elpassion/events";
-        elPassion.members_url = "https://api.github.com/orgs/elpassion/members{/member}";
-        elPassion.public_members_url = "https://api.github.com/orgs/elpassion/public_members{/member}";
-        elPassion.avatar_url = "https://avatars.githubusercontent.com/u/98255?v=3";
-        elPassion.description = "App Development & Design House. We code in Ruby, JavaScript, Swift, Objective-C, Java, Groovy...";
         elPassion.name = "EL Passion";
-        elPassion.company = null;
-        elPassion.blog = "http://www.elpassion.com/";
-        elPassion.location = "Warsaw, Poland";
-        elPassion.email = "hello@elpassion.com";
         elPassion.public_repos = 37;
-        elPassion.public_gists = 0;
         elPassion.followers = 0;
-        elPassion.following = 0;
-        elPassion.html_url = "https://github.com/elpassion";
-        elPassion.created_at = "2009-06-23T18:10:20Z";
-        elPassion.updated_at = "2015-08-08T10:31:55Z";
-        elPassion.type = "Organization";
         return elPassion;
     }
 }
--- app/src/androidTest/java/pl/codepot/groovy_no_way_back/factory/GitHubUserFactory.java
+++ app/src/androidTest/java/pl/codepot/groovy_no_way_back/factory/GitHubUserFactory.java
@@ -9,33 +9,8 @@ public final class GitHubUserFactory {
         ordonTeam.login = "OrdonTeam";
         ordonTeam.id = 4918734L;
         ordonTeam.avatarUrl = "https://avatars.githubusercontent.com/u/4918734?v=3";
-        ordonTeam.gravatarId = "";
-        ordonTeam.url = "https://api.github.com/users/OrdonTeam";
-        ordonTeam.htmlUrl = "https://github.com/OrdonTeam";
-        ordonTeam.followersUrl = "https://api.github.com/users/OrdonTeam/followers";
-        ordonTeam.followingUrl = "https://api.github.com/users/OrdonTeam/following{/other_user}";
-        ordonTeam.gistsUrl = "https://api.github.com/users/OrdonTeam/gists{/gist_id}";
-        ordonTeam.starredUrl = "https://api.github.com/users/OrdonTeam/starred{/owner}{/repo}";
-        ordonTeam.subscriptionsUrl = "https://api.github.com/users/OrdonTeam/subscriptions";
-        ordonTeam.organizationsUrl = "https://api.github.com/users/OrdonTeam/orgs";
-        ordonTeam.reposUrl = "https://api.github.com/users/OrdonTeam/repos";
-        ordonTeam.eventsUrl = "https://api.github.com/users/OrdonTeam/events{/privacy}";
-        ordonTeam.receivedEventsUrl = "https://api.github.com/users/OrdonTeam/received_events";
-        ordonTeam.type = "User";
-        ordonTeam.siteAdmin = false;
         ordonTeam.name = "Karol";
-        ordonTeam.company = null;
-        ordonTeam.blog = null;
-        ordonTeam.location = "Warsaw";
-        ordonTeam.email = "ordon1313@gmail.com";
-        ordonTeam.hireable = null;
-        ordonTeam.bio = null;
-        ordonTeam.publicRepos = 24;
-        ordonTeam.publicGists = 0;
         ordonTeam.followers = 1;
-        ordonTeam.following = 0;
-        ordonTeam.createdAt = "2013-07-02T07:20:33Z";
-        ordonTeam.updatedAt = "2015-08-08T18:43:17Z";
         return ordonTeam;
     }
 }
