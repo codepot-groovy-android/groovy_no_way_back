4651d7ba63210c7a254bf26f04a29c217de1ab82
Maciej Górski <maciek.gorski@gmail.com>
2015-08-29 16:49:53 +0200

    Generator changed to Groovy.

--- app/src/main/groovy/pl/codepot/groovy_no_way_back/random/RandomQueryGenerator.groovy
+++ app/src/main/groovy/pl/codepot/groovy_no_way_back/random/RandomQueryGenerator.groovy
@@ -1,17 +1,18 @@
-package pl.codepot.groovy_no_way_back.random;
+package pl.codepot.groovy_no_way_back.random
 
-import java.util.Random;
+import groovy.transform.CompileStatic
 
-public final class RandomQueryGenerator {
+@CompileStatic
+final class RandomQueryGenerator {
 
     private final Random random = new Random();
-    private final String[] alphabet = new String[]{
+    private final List<String> alphabet = [
             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    };
+    ]
 
-    public String getRandomQuery() {
-        return alphabet[random.nextInt(alphabet.length)]
-                + alphabet[random.nextInt(alphabet.length)];
+    String getRandomQuery() {
+        return alphabet[random.nextInt(alphabet.size())] \
+                 + alphabet[random.nextInt(alphabet.size())]
     }
 }
