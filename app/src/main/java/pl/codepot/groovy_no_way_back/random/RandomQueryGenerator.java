cdb7ab8cf32d93a52091748352d010c6bd2eb5ff
Maciej Górski <maciek.gorski@gmail.com>
2015-08-29 14:10:41 +0200

    Change array of char to String.

--- app/src/main/java/pl/codepot/groovy_no_way_back/random/RandomQueryGenerator.java
+++ app/src/main/java/pl/codepot/groovy_no_way_back/random/RandomQueryGenerator.java
@@ -5,15 +5,13 @@ import java.util.Random;
 public final class RandomQueryGenerator {
 
     private final Random random = new Random();
-    private final char[] alphabet = new char[]{
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    private final String[] alphabet = new String[]{
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     };
 
     public String getRandomQuery() {
-        return new String(new char[]{
-                alphabet[random.nextInt(alphabet.length)],
-                alphabet[random.nextInt(alphabet.length)]
-        });
+        return alphabet[random.nextInt(alphabet.length)]
+                + alphabet[random.nextInt(alphabet.length)];
     }
 }
