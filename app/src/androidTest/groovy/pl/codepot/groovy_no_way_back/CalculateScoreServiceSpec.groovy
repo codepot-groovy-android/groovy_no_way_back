8cfe5bb723fca2e8a20665a43df4e9fa5719981e
ordon <ordon1313@gmail.com>
2015-08-29 18:32:45 +0200

    spock with where and range

--- app/src/androidTest/groovy/pl/codepot/groovy_no_way_back/CalculateScoreServiceSpec.groovy
+++ app/src/androidTest/groovy/pl/codepot/groovy_no_way_back/CalculateScoreServiceSpec.groovy
@@ -5,9 +5,8 @@ import pl.codepot.groovy_no_way_back.api.organization.GitHubUserOrganizationsApi
 import pl.codepot.groovy_no_way_back.api.repo.GitHubUserReposApi
 import pl.codepot.groovy_no_way_back.api.user.GitHubUserApi
 import pl.codepot.groovy_no_way_back.calculator.CalculateScoreService
-import pl.codepot.groovy_no_way_back.factory.GitHubUserFactory
-import rx.Observable
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static pl.codepot.groovy_no_way_back.factory.GitHubUserFactory.newUserWithFollowers
 import static rx.Observable.just;
@@ -19,10 +18,11 @@ final class CalculateScoreServiceSpec extends Specification {
     GitHubUserOrganizationsApi gitHubUserOrganizationsApi = Mock(GitHubUserOrganizationsApi)
     GitHubOrganizationApi gitHubOrganizationApi = Mock(GitHubOrganizationApi)
 
-    def "Should calculate score exuals to 0"() {
+    @Unroll
+    def "Should calculate score exuals to #followers"() {
         given:
         CalculateScoreService service = new CalculateScoreService(gitHubUserApi, gitHubUserReposApi, gitHubUserOrganizationsApi, gitHubOrganizationApi)
-        gitHubUserApi.get(_) >> just(newUserWithFollowers(0))
+        gitHubUserApi.get(_) >> just(newUserWithFollowers(followers))
         gitHubUserReposApi.get(_) >> just([])
         gitHubUserOrganizationsApi.get(_) >> just([])
         when:
@@ -31,7 +31,9 @@ final class CalculateScoreServiceSpec extends Specification {
             score = it
         }
         then:
-        score == 0
+        score == followers
+        where:
+        followers << (0..<100)
     }
 
 }
